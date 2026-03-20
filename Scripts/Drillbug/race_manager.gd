90406dd7b6cb256473a1a32a5d5c4ecb3c691a72
YeePomfPomf <131655763+yeepomfpomf@users.noreply.github.com>
2026-03-20 14:52:02 +1100

    vendor logic

--- Scripts/Drillbug/race_manager.gd
+++ Scripts/Drillbug/race_manager.gd
@@ -1,10 +1,13 @@
 extends Node3D
 
-
+@export var player: Player
 @export var starting_positions: Array[Node3D]
 
 @export var participating_bugs: Array[DrillBug]
+@onready var label_3d: Label3D = $Kiosk/Label3D
 
+var bet_amount: int = 0
+var bug_betted_on: DrillBug
 
 func _ready() -> void:
 	prepare_race()
@@ -17,6 +20,26 @@ func prepare_race() -> void:
 		
 		i += 1
 		
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(5).timeout
 	for bug in participating_bugs:
 		bug.start_race()
+
+func place_bet(amount: int, bug: DrillBug) -> void:
+	bet_amount = amount
+	bug_betted_on = bug
+
+func race_win(winning_bug: DrillBug) -> void:
+	if winning_bug == bug_betted_on:
+		player.add_ticket(bet_amount * 2)
+	
+
+func _on_finish_line_body_entered(body: Node3D) -> void:
+	if body is DrillBug:
+		var bug: DrillBug = body
+		if global_basis.z.dot((global_position - bug.global_position).normalized()) > 0:
+			bug.current_lap += 1
+			print(bug, " is on lap ", bug.current_lap)
+			if bug.current_lap == 3:
+				race_win(bug)
+				for i in participating_bugs:
+					i.racing = false
